c452552c425720e4470fad20759da8bcc0efa587
Martin Ahlström <martin.ahlstrom@outlook.com>
2016-04-11 11:41:02 +0200

    Add models for generating report

---
+++ TrelloWindow/PrintCompactModels/Card.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TrelloWindow.PrintCompactModels
+{
+	internal class Card
+	{
+		public Board Board { get; set; }
+		public string Description { get; set; }
+		public string DueDate { get; set; }
+		public IEnumerable<Label> Labels { get; set; }
+		public BoardList List { get; set; }
+		public IEnumerable<Member> Members { get; set; }
+		public string Name { get; set; }
+	}
+}
---
+++ TrelloWindow/PrintCompactModels/Member.cs
@@ -0,0 +1,11 @@
+﻿namespace TrelloWindow.PrintCompactModels
+{
+	internal class Member
+	{
+		public string Avatar30Px => string.IsNullOrWhiteSpace(AvatarHash) ? "" : $"https://trello-avatars.s3.amazonaws.com/{AvatarHash}/30.png";
+		public string AvatarHash { get; set; }
+		public string Initials { get; internal set; }
+		public string Name { get; internal set; }
+		public string Username { get; internal set; }
+	}
+}
